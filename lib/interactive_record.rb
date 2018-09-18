f5d3eda3e97a9eea0eacfaa4ba13dc797572c2dc
Richard Sanchez <richard.charles.s@gmail.com>
2018-09-18 19:25:08 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -17,7 +17,12 @@ class InteractiveRecord
     table_info.each do |column|
       column_names << column["name"]
     end
-
     column_names.compact
+  end
+
+  def initialize(options={})
+    options.each do |property, value|
+      self.send("#{property}=", value)
+    end
   end
 end
